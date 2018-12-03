7a540657d9ae42428aa1a5138911d2ea4f2df77d
apetriceanu <apetriceanu@live.com>
2018-12-03 09:52:46 +0100

    added lights

--- Pedestrian-Crossing-Lights/main.asm
+++ Pedestrian-Crossing-Lights/main.asm
@@ -20,4 +20,29 @@ delay_loop_3:
 	brne delay_loop_1	; total delay in ms = cycles/16000 = 49940219/16000 = 3.121,2636875ms = 3.121s
 .endmacro
 
+.macro redLight
+    ldi r16, 0b10000000        ;red light on 13 pin (pinb 1000 0000)
+    out portb, r16
+.endmacro
+.macro yellowLight
+    ldi r16, 0b01000000        ;yellow light on 12 pin (pinb 0100 0000)
+    out portb, r16
+.endmacro
+.macro greenLight
+    ldi r16, 0b00100000        ;green light on 11 pin (pinb 0010 0000)
+    out portb, r16
+.endmacro
+.macro yellowRed
+    ldi r16, 0b11000000        ; yellow AND red light on pins 12 and 13 (pinb 1100 0000)
+    out portb, r16
+.endmacro
+.macro pedGreen
+    ldi r16, 0b10010000        ;pedestrian green light on pin 10 (pinb 0001 0000)
+    out portb, r16            ;also keeps red light on
+.endmacro
 
+
+init:
+    ldi r16, 0b11110000        ;set all pins of portb (pinb 1111 0000) to outputs
+    out ddrb, r16
+    out portb, r16
